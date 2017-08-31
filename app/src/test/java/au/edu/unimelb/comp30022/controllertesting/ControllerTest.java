257d981d82e752cda3e278dcb1452efec22568fa
williamvoor <william.v@unimelb.edu.au>
2017-08-31 16:05:55 +1000

    Added test folder and empty ControllerTest

---
+++ app/src/test/java/au/edu/unimelb/comp30022/controllertesting/ControllerTest.java
@@ -0,0 +1,22 @@
+package au.edu.unimelb.comp30022.controllertesting;
+
+import org.junit.After;
+import org.junit.Before;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by william on 31/8/17.
+ */
+public class ControllerTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+}
